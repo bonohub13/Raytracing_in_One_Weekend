74e9f33b7c6cf673a0ae007152112cda03f3f2bd
Kensuke Saito <ken.saito.0813@gmail.com>
2025-12-26 08:56:30 +0900

    Implemented vec3

---
+++ include/vec3.h
@@ -0,0 +1,27 @@
+#ifndef RTIOW_VEC3_H
+#define RTIOW_VEC3_H
+
+#define VEC3(_x, _y, _z)        ((st_vec3_t){ { (_x), (_y), (_z), 0 } })
+#define VEC3_ZERO               (VEC3(0, 0, 0))
+#define VEC3_ONE                (VEC3(1, 1, 1))
+#define VEC3_WHITE              (VEC3_ONE)
+#define VEC3_BLACK              (VEC3_ZERO)
+
+typedef struct vec3 {
+    double e[4] __attribute__((aligned(32)));
+} st_vec3_t;
+
+extern st_vec3_t vec3_neg(const st_vec3_t * const p_v);
+extern double vec3_length(const st_vec3_t * const p_v);
+extern double vec3_length_squared(const st_vec3_t * const p_v);
+extern char * vec3_string(const st_vec3_t * const p_v);
+extern st_vec3_t vec3_add(const st_vec3_t * const p_u, const st_vec3_t * const p_v);
+extern st_vec3_t vec3_sub(const st_vec3_t * const p_u, const st_vec3_t * const p_v);
+extern st_vec3_t vec3_mul(const st_vec3_t * const p_u, const st_vec3_t * const p_v);
+extern st_vec3_t vec3_scalar_mul(const st_vec3_t * const p_v, const double t);
+extern st_vec3_t vec3_scalar_div(const st_vec3_t * const p_v, const double t);
+extern double vec3_dot(const st_vec3_t * const p_u, const st_vec3_t * const p_v);
+extern st_vec3_t vec3_cross(const st_vec3_t * const p_u, const st_vec3_t * const p_v);
+extern st_vec3_t vec3_unit_vector(const st_vec3_t * const p_v);
+
+#endif /* RTIOW_VEC3_H */
---
+++ src/vec3.c
@@ -0,0 +1,88 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "vec3.h"
+
+#define BUFFER_SIZE         (0x10)
+#define STRING_LENGTH       (15)
+
+st_vec3_t vec3_neg(const st_vec3_t * const p_v) {
+    return VEC3(-p_v->e[0], -p_v->e[1], -p_v->e[2]);
+}
+
+double vec3_length(const st_vec3_t * const p_v) {
+    return sqrt(vec3_length_squared(p_v));
+}
+
+double vec3_length_squared(const st_vec3_t * const p_v) {
+    return vec3_dot(p_v, p_v);
+}
+
+char * vec3_string(const st_vec3_t * const p_v) {
+    static uint8_t s_buffer[BUFFER_SIZE] = { 0 };
+    int32_t capacity = snprintf((char*)s_buffer, BUFFER_SIZE, "%.2f %.2f %.2f",
+            p_v->e[0], p_v->e[1], p_v->e[2]);
+
+    if (STRING_LENGTH == capacity) {
+        return (char*)s_buffer;
+    }
+
+    return NULL;
+}
+
+st_vec3_t vec3_add(const st_vec3_t * const p_u, const st_vec3_t * const p_v) {
+    return VEC3(
+            p_u->e[0] + p_v->e[0],
+            p_u->e[1] + p_v->e[1],
+            p_u->e[2] + p_v->e[2]);
+}
+
+st_vec3_t vec3_sub(const st_vec3_t * const p_u, const st_vec3_t * const p_v) {
+    return VEC3(
+            p_u->e[0] - p_v->e[0],
+            p_u->e[1] - p_v->e[1],
+            p_u->e[2] - p_v->e[2]);
+}
+
+st_vec3_t vec3_mul(const st_vec3_t * const p_u, const st_vec3_t * const p_v) {
+    return VEC3(
+            p_u->e[0] * p_v->e[0],
+            p_u->e[1] * p_v->e[1],
+            p_u->e[2] * p_v->e[2]);
+}
+
+st_vec3_t vec3_scalar_mul(const st_vec3_t * const p_v, const double t) {
+    return VEC3(
+            t * p_v->e[0],
+            t * p_v->e[1],
+            t * p_v->e[2]);
+}
+
+st_vec3_t vec3_scalar_div(const st_vec3_t * const p_v, const double t) {
+    return vec3_scalar_mul(p_v, 1.0 / t);
+}
+
+double vec3_dot(const st_vec3_t * const p_u, const st_vec3_t * const p_v) {
+    st_vec3_t out = vec3_mul(p_u, p_v);
+
+    return out.e[0] + out.e[1] + out.e[2];
+}
+
+st_vec3_t vec3_cross(const st_vec3_t * const p_u, const st_vec3_t * const p_v) {
+    st_vec3_t tmp[4] = {
+        VEC3(p_u->e[1], p_u->e[2], p_u->e[0]),
+        VEC3(p_v->e[2], p_v->e[0], p_v->e[1]),
+        VEC3(p_u->e[2], p_u->e[0], p_u->e[1]),
+        VEC3(p_v->e[1], p_v->e[2], p_v->e[0]),
+    };
+
+    tmp[0] = vec3_mul(&tmp[0], &tmp[1]);
+    tmp[2] = vec3_mul(&tmp[2], &tmp[3]);
+
+    return vec3_sub(&tmp[0], &tmp[2]);
+}
+
+st_vec3_t vec3_unit_vector(const st_vec3_t * const p_v) {
+    return vec3_scalar_div(p_v, vec3_length(p_v));
+}
