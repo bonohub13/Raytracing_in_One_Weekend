b4192582eceefaba3e23f357495ae7bde71157b6
Kensuke Saito <ken.saito.0813@gmail.com>
2025-12-29 17:04:52 +0900

    Implemented RNG functions

--- include/rtweekend.h
+++ include/rtweekend.h
@@ -6,6 +6,8 @@
 #define PI                  (3.1415926535897932385)
 
 extern double degrees_to_radians(double degrees);
+extern double random_double(void);
+extern double random_double_in_range(double min, double max);
 extern size_t round_up(size_t val, size_t div);
 
 #endif /* RTIOW_RTWEEKEND_H */
--- src/rtweekend.c
+++ src/rtweekend.c
@@ -1,7 +1,26 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+
 #include "rtweekend.h"
 
 double degrees_to_radians(double degrees) {
     return degrees * PI / 180.0;
+}
+
+double random_double(void) {
+    static uint32_t s_seed = 0;
+
+    if (0 == s_seed) {
+        s_seed = time(NULL);
+        srand(s_seed);
+    }
+
+    return rand() / (RAND_MAX + 1.0);
+}
+
+double random_double_in_range(double min, double max) {
+    return min + (max - min) * random_double();
 }
 
 size_t round_up(size_t val, size_t div) {
